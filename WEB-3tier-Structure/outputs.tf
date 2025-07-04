25a8778f03c7e59c4eae854b4ccb50e94b4a416f
gogogojun <123915210+gogogojun@users.noreply.github.com>
2025-07-04 17:49:00 +0900

    Delete WEB-3tier-Structure directory

--- WEB-3tier-Structure/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "db_dns_name" {
-  description = "db_dns_name"
-  value = aws_lb.myALB.dns_name
-}
--- WEB-3tier-Structure/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "AZ-1" {
-    default = "ap-northeast-2a"
-}
-
-variable "AZ-2" {
-    default = "ap-northeast-2c"
-}
